d851145656d2585df25e665051ba32894dfc7b85
Mwesigye_Nicholas <nicholastoshack12345@gmail.com>
2026-03-03 21:28:32 +0300

    Last challenge day one

---
+++ 01_basics/03_list_less_than_10.py
@@ -0,0 +1,9 @@
+
+numbers =  [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+numbers_less_than_10 = []
+
+for number in numbers:
+    if number < 10:
+        numbers_less_than_10.append(number)
+print(numbers_less_than_10)
